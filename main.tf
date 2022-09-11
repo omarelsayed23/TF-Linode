62f283e7339f209792f40169f7e878bf25ee641e
omarelsayed23 <108306241+omarelsayed23@users.noreply.github.com>
2022-09-11 03:35:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,18 +16,50 @@ resource "linode_stackscript" "juno_stackscript" {
   label = "juno_node"
   description = "Run a juno node"
 
-  images = ["linode/ubuntu18.04", "linode/ubuntu16.04lts"]
+  images = ["linode/ubuntu18.04", "linode/ubuntu16.04lts", "linode/ubuntu22.04"]
   rev_note = "initial version"
   script = <<EOF
 #!/bin/sh
-mkdir omar
-echo "omar is here"
+#!/bin/bash
+# codenoid
+# https://gist.github.com/codenoid/4806365032bb4ed62f381d8a76ddb8e6
+mkdir /root/omar
+touch /root/test.txt
+echo "Script starts"
+printf "Checking latest Go version...\n";
+LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text)";
+LATEST_GO_DOWNLOAD_URL="https://golang.org/dl/1.19.linux-amd64.tar.gz "
+
+printf "cd to home ($USER) directory \n"
+cd "/home/$USER"
+
+curl -OJ -L --progress-bar https://golang.org/dl/1.19.linux-amd64.tar.gz
+
+printf "Extracting file...\n"
+tar -xf 1.19.linux-amd64.tar.gz
+
+
+latest="$(echo $url | grep -oP 'go[0-9\.]+' | grep -oP '[0-9\.]+' | head -c -2 )"
+
+
+echo "Create the skeleton for your local users go directory"
+mkdir -p ~/go/{bin,pkg,src}
+echo "Setting up GOPATH"
+echo "export GOPATH=~/go" >> ~/.profile && source ~/.profile
+echo "Setting PATH to include golang binaries"
+echo "export PATH='$PATH':/usr/local/go/bin:$GOPATH/bin" >> ~/.profile && source ~/.profile
+echo "Installing dep for dependency management"
+go get -u github.com/golang/dep/cmd/dep
+
+
+printf "You are ready to Go!\n";
+go version
 EOF
 }
 
 
 resource "linode_instance" "juno_node" {
-  image  = "linode/ubuntu18.04"
+  image  = "linode/ubuntu22.04"
   label  = "juno"
   region = "us-east"
   type   = "g6-standard-2"
