d30a165f4520d8cb1dab22cac9aa163771545216
menna161 <57011308+menna161@users.noreply.github.com>
2022-09-17 00:23:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -115,7 +115,7 @@ resource "linode_instance" "juno_node" {
   region = "us-east"
   type   = "g6-standard-2"
 #   authorized_keys    = [var.authorized_keys]
-  authorized_keys    = "hello"
+  authorized_keys    = ["hello"]
 #   authorized_keys    = [${{ secrets.authorized_keys }}]
 #   root_pass      = var.root_pass
   root_pass      = "hello"
