18f4576f55cf6b4c5d442c2171427c8ce245ace6
omarelsayed23 <108306241+omarelsayed23@users.noreply.github.com>
2022-09-19 04:17:39 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,3 +4,9 @@ variable "root_pass" {}
 variable "region" {
   default = "us-southeast"
 }
+
+variable "nodes_script_file" {
+  type        = string
+  description = "Script file path"
+  default     = "./run-juno.sh"
+}
