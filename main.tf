dd50f18e97728d80cbafb52dadff977064ee8ad9
omarelsayed23 <108306241+omarelsayed23@users.noreply.github.com>
2022-09-21 14:23:21 +0200

    Removed duplicated region + removed $ from vars

--- main.tf
+++ main.tf
@@ -23,7 +23,6 @@ provider "aws" {
   region = "us-east-1"
 access_key = TF_VAR_AWS_ACCESS_KEY_ID
 secret_key = TF_VAR_AWS_SECRET_ACCESS_KEY
-region = "us-east-1"
 }
 
 ################ New Part for Secrets ##################
