060128e7573883846e721685bea72cb3bd8b2700
omarelsayed23 <108306241+omarelsayed23@users.noreply.github.com>
2022-09-21 23:39:43 +0200

    Create variables_mainnet.tf

---
+++ variables_mainnet.tf
@@ -0,0 +1,14 @@
+/*
+variable "token" {}
+variable "authorized_keys" {}
+variable "root_pass" {}
+*/
+variable "region" {
+  default = "us-southeast"
+}
+
+variable "nodes_script_file" {
+  type        = string
+  description = "Script file path"
+  default     = "./run-juno-mainnet.sh"
+}
